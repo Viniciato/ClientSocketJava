b66b0f73e0d3156b382228c5b1882d07feb92fdb
Vinicius N <vininadin@gmail.com>
2016-10-07 12:42:09 -0300

    Created connection method on client

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,4 +1,26 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 public class Controller {
+    @FXML private TextField ipLabel;
+    @FXML private TextField portLabel;
+    @FXML private TextField nameLabel;
+
+    @FXML void connectAction(ActionEvent event) throws IOException{
+        Socket socket = new Socket(ipLabel.getText(), Integer.parseInt(portLabel.getText()));
+        OutputStream outPutStream = socket.getOutputStream();
+        OutputStreamWriter outStreamWriter = new OutputStreamWriter(outPutStream);
+        BufferedWriter buffeWriter = new BufferedWriter(outStreamWriter);
+        buffeWriter.write(nameLabel.getText()+"\r\n");
+        buffeWriter.flush();
+    }
 }
