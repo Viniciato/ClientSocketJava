ecaa1d60b00e1b8c9b4f13e0eeebff25e5ca5546
Vinicius N <vininadin@gmail.com>
2016-10-07 13:50:50 -0300

    Created methods to send messages client to server

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -2,7 +2,12 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -23,5 +28,19 @@ public class Controller {
         buffeWriter.write(nameLabel.getText()+"\r\n");
         buffeWriter.flush();
         System.out.println("Connected on " + socket.getInetAddress());
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/chatWindow.fxml"));
+        Parent home_page_parent = loader.load();
+
+        ChatWindowController controller = loader.getController();
+        controller.setParams(buffeWriter, nameLabel.getText());
+
+        Scene home_page_scene = new Scene(home_page_parent);
+        Stage main_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        main_stage.close();
+        main_stage.setScene(home_page_scene);
+        main_stage.setResizable(false);
+        main_stage.setMaximized(false);
+        main_stage.show();
     }
 }
